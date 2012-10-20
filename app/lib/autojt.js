3d9b46faa67fc2fc4bb0ae4722560d4b5700836a
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2012-10-20 19:11:07 +0200

    Data proxy and FLV retrieval ok.

--- app/lib/autojt.js
+++ app/lib/autojt.js
@@ -1,16 +1,117 @@
-define(['underscore'],function(_){
+define(['underscore','jquery', 'jquery.getfeed'],function(_, $, getfeed){
 	
-	function generate_xml(iArg)
+	function generate_xml(iFlvFiles) {
+		_.each(iFlvFiles, function(elem){
+			console.log(elem);
+		});
+	}
+
+	function get_flv_list(iWeights, iDuration, iFeeds)
 	{
+		var flv_files = [];
+		var nb_movies = Math.floor(iDuration);
+		var count = 0;
+		var movies = {};
+
+		_.each(iWeights, function(value,index){
+			var nb = Math.floor(value*nb_movies/5);
+			iWeights[index] = nb;
+			count += nb;
+		});
+
+		_.each(iWeights, function(value,index){
+			var nb = value;
+			var feed = iFeeds[index];
+			var items = [];
+			var index = 0;
+			for(index = 0; index < nb; ++index) {
+				$.ajax({
+					url: 'http://localhost:1235/?url=' + encodeURIComponent(feed.items[index].link),
+					type: 'GET',
+					timeout:5000,
+					error: function(){
+						--count;
+						if(0 === count){
+               				generate_xml(flv_files);
+               			}
+					},
+					success:function(data) {
+						var str = "videofile:\"flv";
+						var flv = '';
+						var idx, idx2;
+						
+						idx = data.indexOf(str);
+						if(-1 != idx) {
+							idx2 = data.indexOf("\"",idx+str.length);
+                        	// http://video.euronews.com/flv/new/cut/eco/121019_BUSU_180A0_F.flv
+                        	flv = "http://video.euronews.com/flv"+data.substring(idx+str.length,idx2)+".flv";
+                			//$("#player").html('</br>'+flv);
+               		 		flv_files.push(flv);
+               		 	}
+               		 	--count;
+               			if(0 === count){
+               				generate_xml(flv_files);
+               			}
+					}
+				});
+			}
+		});
+	}
+
+
+	function retrieve_feeds(iThemes, iDuration)
+	{
+		var feeds = {};
 		var datas = {};
-		_.each(iArg,function(value,id){
+		_.each(iThemes,function(value,id){
 			datas[id] = Math.floor(value/20);
 		});
-		console.log(datas);
+
+		var toBrowse = {
+			business: 'http://feeds.feedburner.com/euronews/fr/business?format=xml',
+			euro: 'http://feeds.feedburner.com/euronews/fr/europa?format=xml',
+			science:'http://feeds.feedburner.com/euronews/fr/sci-tech?format=xml',
+			culture:'http://feeds.feedburner.com/euronews/fr/lifestyle?format=xml'
+		};
+
+		var count = _.reduce(datas, function(memo, elem){
+			return memo + ((0 < elem) ? 1 : 0);
+		},0);
+
+
+		_.each(datas, function(elem, index){
+			if(0 < elem) {
+				$.getFeed({
+		          url: 'http://localhost:1235/?url=' + encodeURIComponent(toBrowse[index]),
+		          success: function(feed) {
+		            feeds[index] = feed;
+		            --count;
+					if(0 === count) {
+						get_flv_list(datas, iDuration, feeds);
+					}
+		          },
+		          error:function(allo) {
+		            console.log(allo);
+		          }
+				});
+			}
+		});
+
+        /*$.getFeed({
+          url: 'http://localhost:1235/?url=' + encodeURIComponent('http://feeds.feedburner.com/euronews/fr/sci-tech?format=xml'),
+          success: function(feed) {
+            feeds.push(feed);
+          },
+          error:function(allo) {
+            console.log(allo);
+          }
+      	});*/
+
+
 		return "<!-- -->";
 	};
 
 	return {
-		getStupeflixXML:generate_xml
+		generateStupeflixXML:retrieve_feeds
 	};
 });
---
+++ proxy/node/dataproxy.js
@@ -0,0 +1,40 @@
+var express = require('express'),
+app = express();
+var http = require('http');
+var restler = require('restler');
+var port = process.env.PORT || 1235;
+
+console.log("Listening on " + port);
+app.use(express.bodyParser());
+app.listen(port);
+
+app.get('/index.html', function(req, res) {
+    res.sendfile(__dirname + '/app/app.html');
+});
+
+app.get('/lib/*', function(req, res) {
+    res.sendfile(__dirname + '/app' + req.url);
+});
+app.get('/css/*', function(req, res) {
+    res.sendfile(__dirname + '/app' + req.url);
+});
+
+app.all('/*', function(req, res) {
+    req.url = req.query.url;
+    console.log(req.url);
+    restler.get(req.url, {}).on('complete', function (data) {
+        //res.set('Content-Type', 'application/xml');
+        res.send(data);
+    });
+
+    /*http.get(req.url, function(response) {
+        //console.log(response);
+        //res.set('Content-Type', response.headers['Content-Type']);
+        res.set('Content-Type', 'application/xml');
+        response.on("data", function(text) {
+            console.log(text);
+        });
+    }).on('error', function(e) {
+        console.log("Got error: " + e.message);
+    });*/
+});
