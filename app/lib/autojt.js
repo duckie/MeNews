b6a158b66c8023641fbb792ff8fe82d973d4566a
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2012-10-21 14:30:08 +0200

    Video transition.

--- app/lib/autojt.js
+++ app/lib/autojt.js
@@ -1,14 +1,26 @@
 define(['underscore','jquery', 'jquery.getfeed'],function(_, $, getfeed){
     
     function generate_xml(iFlvFiles, iTrans) {
-        var stupeflix_xml = '<!-- '
 
-        stupeflix_xml += '<effect type="none"><video filename="https://dl.dropbox.com/u/20904373/FINAL_generique.wmv" /></effect>';
+
+        var stupeflix_xml = '<!-- ';
+
+        // Generique
+        //stupeflix_xml += '<effect type="none"><video filename="https://dl.dropbox.com/u/20904373/FINAL_generique.wmv" /></effect>';
+        stupeflix_xml += '<stack><overlay width="1.0" height="1.33333333" top="auto" bottom="auto"><video filename="https://dl.dropbox.com/u/20904373/FINAL_generique.wmv"/></overlay></stack>';
+        stupeflix_xml += '<transition type="crossfade" duration="1" /><overlay duration="3.0"><image color="#000000"/></overlay><transition type="crossfade" duration="1" />'
 
         _.each(iFlvFiles, function(elem){
             //stupeflix_xml += '<effect type="explode" duration="20"> <video filename="'+elem+'"/> </effect> <transition type="move" direction="left" duration="2"/>'
-            stupeflix_xml += '<effect type="explode" duration="20"><video filename="'+elem+'" /></effect>'
+            
+            // Video
+            stupeflix_xml += '<effect type="none" duration="20.0"><video filename="'+elem+'" /></effect>'
+
+            // Transition
+            stupeflix_xml += '<transition type="crossfade" duration="1" /><overlay duration="3.0"><image color="#000000"/></overlay><transition type="crossfade" duration="1" />'
         });
+
+        stupeflix_xml += '<overlay duration="5.0"><image color="#000000"/></overlay>';
         stupeflix_xml += ' -->';
 
         $('#moviexml').append(stupeflix_xml);
