acd58b0bc0fc3dd106a9477da94585188a03568f
Jean-Bernard Jansen <jeanbernard.jansen@gmail.com>
2012-10-21 03:31:59 +0200

    Cleansing.

--- app/dataproxy.js
+++ app/dataproxy.js
@@ -9,36 +9,13 @@ console.log("Listening on " + port);
 app.use(express.static(__dirname));
 app.listen(port);
 
-
 app.get('/', function(req, res) {
     res.sendfile(__dirname + '/app.html');
 });
 
-app.get('/index.html', function(req, res) {
-    res.sendfile(__dirname + '/app.html');
-});
-/*app.get('/lib/*', function(req, res) {
-    res.sendfile(__dirname + req.url);
-});
-app.get('/css/*', function(req, res) {
-    res.sendfile(__dirname + req.url);
-});*/
-
 app.get('/proxy', function(req, res) {
     req.url = req.query.url;
     restler.get(req.url, { headers: {'Referer':null, 'X-Requested-With':null} }).on('complete', function (data) {
-        //res.set('Content-Type', 'application/xml');
         res.send(data);
     });
-
-    /*http.get(req.url, function(response) {
-        //console.log(response);
-        //res.set('Content-Type', response.headers['Content-Type']);
-        res.set('Content-Type', 'application/xml');
-        response.on("data", function(text) {
-            console.log(text);
-        });
-    }).on('error', function(e) {
-        console.log("Got error: " + e.message);
-    });*/
 });
